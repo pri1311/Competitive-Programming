0cb3380f252a9aa47971aedb8cbff338e1cf0735
Priya Nagda <pri1311@users.noreply.github.com>
2021-04-04 19:07:06 +0530

    daa a3

--- DynamicProgramming/optimalSchedule.cpp
+++ DynamicProgramming/optimalSchedule.cpp
@@ -3,12 +3,30 @@ using namespace std;
  
 int optimalSchedule(int a[], int n, int r, int c){
     int opt[n+1];
+    char sc[n+1];
     for (int i = 0; i < 4; i++){
+        sc[i] = 'A';
         opt[i] = r * a[i];
     }
     for (int i = 4; i < n+1; i++){
         opt[i] = min(opt[i-1] + r * a[i], opt[i-4] + 4 * c);
+        if (opt[i-4] + 4*c == opt[i] && opt[i-1] + r*a[i] != opt[i]){
+            sc[i-1] = sc[i-2] = sc[i-3] = sc[i] = 'B';
+            if (i>=7 && sc[i-7]!='B'){
+                sc[i-4] = 'A';
+                sc[i-5] = 'A';
+                sc[i-6] = 'A';
+            }
+        }
+        else{
+            sc[i] = 'A';
+        }
     }
+    cout<<"Schedule:\n";
+    for (int i = 1; i < n+1; i++){
+        cout<<sc[i]<<" ";
+    }
+    cout<<endl;
     return opt[n];
 }
 
@@ -24,6 +42,7 @@ int main() {
     for (int i = 1; i < n+1; i++){
         cin>>a[i];
     }
-    cout<<optimalSchedule(a, n, r, c);
+    int result = optimalSchedule(a, n, r, c);
+    cout<<"Maximum profit: "<<result;
     return 0;
 }
--- DynamicProgramming/triangleMaxSum.cpp
+++ DynamicProgramming/triangleMaxSum.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> dp;
+
 int getHeight(int i) {
     int a = 0;
     int c = 0;
     while (true){
         c ++;
         if (i >= a && i < 2*a +1){
-            break;
             return c;
         }
         else{
@@ -22,8 +23,17 @@ int maxSum(int a[], int index, int n) {
     if (index <(n*(n+1))/2) {
         m = (n*(n-1))/2;
         int j = getHeight(index);
-        if (index >= m) return a[index];
-        else return a[index] + max(maxSum(a,index +j,n), maxSum(a,index + j+1,n));
+        if (index >= m) return dp[index] = a[index];
+        
+        else{
+            if (dp[index+j] == -1){
+                dp[index + j] = maxSum(a,index +j,n);
+            }
+            if (dp[index+j+1] == -1){
+                dp[index + j +1] = maxSum(a,index +j+1,n);
+            }
+            return dp[index] = a[index] + max(dp[index + j], dp[index + j +1]);
+        }
     }
     else return 0;
 }
@@ -35,6 +45,7 @@ int main() {
     cin>>n;
     int m = (n*(n+1))/2;
     int a[m];
+    dp.assign(m,-1);
     for(int i = 0; i < m; i++){
         cin>>a[i];
     }
